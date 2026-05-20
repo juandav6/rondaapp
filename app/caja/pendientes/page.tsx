4cf8f2656780b833590ca8b9c1d04d0de3a23c9e
juandav6 <juanda61217@gmail.com>
2026-05-20 16:18:39 -0500

    Update page.tsx

--- app/caja/pendientes/page.tsx
+++ app/caja/pendientes/page.tsx
@@ -90,7 +90,7 @@ export default function CajaPendientesPage() {
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="h-4 w-4 shrink-0 mt-0.5 text-blue-500">
           <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 0 1 .67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 1 1-.671-1.34l.041-.022ZM12 9a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" clipRule="evenodd"/>
         </svg>
-        <span>Al cobrar un préstamo express, el interés generado se registra automáticamente como ingreso en la caja común y se distribuye entre los inversores.</span>
+        <span>Al cobrar un préstamo express, el interés generado entra automáticamente a la <strong>caja común</strong> y queda disponible para gastos de la ronda.</span>
       </div>
 
       {pendientes.length === 0 ? (
